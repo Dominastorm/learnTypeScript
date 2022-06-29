28fa3d349daecc4ce21d49dd867db768a72a19a1
Dhruv Chawla <dchawla228@gmail.com>
2022-06-29 10:56:44 +0530

    added task done functionality (strikethrough)

--- typescript-tutorial/src/components/SingleTodo.tsx
+++ typescript-tutorial/src/components/SingleTodo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Todo } from "../model";
-import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
-import { MdDone } from 'react-icons/md';
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 import "./styles.css";
 
 type Props = {
@@ -11,14 +11,34 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDelete = () => {};
+  const handleEdit = () => {};
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
     <div>
       <form className="todos__single">
-        <span className="todos__single--text">{todo.todo}</span>
+        {todo.isDone ? (
+          <s className="todos__single--text">{todo.todo}</s>
+        ) : (
+          <span className="todos__single--text">{todo.todo}</span>
+        )}
         <div>
-            <span className="icon"><AiFillEdit/></span>
-            <span className="icon"><AiFillDelete/></span>
-            <span className="icon"><MdDone/></span>
+          <span className="icon">
+            <AiFillEdit />
+          </span>
+          <span className="icon">
+            <AiFillDelete />
+          </span>
+          <span className="icon" onClick={() => handleDone(todo.id)}>
+            <MdDone />
+          </span>
         </div>
       </form>
     </div>
